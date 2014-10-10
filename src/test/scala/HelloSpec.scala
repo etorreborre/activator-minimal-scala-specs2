2aab4a6a62d3fca0378ab85614452a0ad54788d2
Eric Torreborre <etorreborre@yahoo.com>
2014-10-10 15:12:05 +1100

    small commit to test the webhook

--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -1,8 +1,12 @@
 import org.specs2._
 
 class HelloSpec extends Specification { def is = s2"""
+
   Hello should be specified
-    Hello does ... $hello
+   Hello does ... $hello
+
 """
+
   def hello = pending    
+
 }
